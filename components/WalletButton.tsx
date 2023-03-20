a9d3263cff5fa0d1d78313e85a0dd00d6c926141
Madusha Prasanjith <hi@madusha.me>
2023-03-20 13:07:10 +0100

    fix: WalletButton z-index styles.

--- components/WalletButton.tsx
+++ components/WalletButton.tsx
@@ -1,22 +1,21 @@
-import { Flex } from '@mantine/core';
-import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { FC } from 'react'
+import { Box, Flex } from "@mantine/core";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { FC } from "react";
 
 export const WalletButton: FC = () => {
   return (
-    <>
-      <Flex
-        display="flex"
-        direction={'row'}
-        justify={'flex-end'}
-        p={'16px'}
-        align={'center'}
-        mt={0}
-        bg={'#f0f3f5'}
-        gap={'1px solid #E5E5E5'}
-      >
-          <ConnectButton label="Connect" />
-      </Flex>
-    </>
-  )
-}
+    <Flex
+      sx={{ position: "relative", zIndex: 1000 }}
+      display="flex"
+      direction={"row"}
+      justify={"flex-end"}
+      p={"16px"}
+      align={"center"}
+      mt={0}
+      bg={"#f0f3f5"}
+      gap={"1px solid #E5E5E5"}
+    >
+      <ConnectButton label="Connect" />
+    </Flex>
+  );
+};
