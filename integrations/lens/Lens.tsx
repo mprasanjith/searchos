6f780c7eeb0f65982b4a3bcdd9fee8fdccc2d40e
Madusha Prasanjith <hi@madusha.me>
2023-03-25 13:12:15 +0100

    feat: Update Lens integration.

--- integrations/lens/Lens.tsx
+++ integrations/lens/Lens.tsx
@@ -1,20 +1,10 @@
-import {
-  Box,
-  CommandHandlerProps,
-  Detail,
-  Group,
-  Stack,
-  Avatar,
-  Text,
-} from "@/sdk";
+import { Box, CommandHandlerProps, Detail, Group, Stack, Text } from "@/sdk";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 import icon from "./icon.png";
 import IconHeader from "@/sdk/templates/IconHeader";
-import DetailsGrid from "@/sdk/templates/DetailsGrid";
 import { ProfileFragment } from "@lens-protocol/client";
-import LensClient, {
-  Profile,
-} from "@lens-protocol/client/dist/declarations/src";
+import LensClient from "@lens-protocol/client/dist/declarations/src";
 
 interface LensProfileProps extends CommandHandlerProps {
   query: string;
@@ -22,42 +12,20 @@ interface LensProfileProps extends CommandHandlerProps {
 }
 
 const LensProfile: React.FC<LensProfileProps> = ({ client, query }) => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setIsError] = useState<boolean>(false);
-  const [profile, setProfile] = useState<ProfileFragment | null>(null);
+  const {
+    data: profile,
+    isLoading,
+    error,
+  } = useSWR(`lens/profile/${query}`, async () => {
+    return client.profile.fetch({ handle: query });
+  });
 
-  useEffect(() => {
-    if (!query) return;
-
-    setIsLoading(true);
-    setIsError(false);
-    let active = true;
-
-    async function getProfile() {
-      const result = await client.profile.fetch({ handle: query });
-      if (!result) return;
-
-      if (active) {
-        !result ? setIsError(true) : setProfile(result);
-        setIsLoading(false);
-      }
-    }
-
-    getProfile();
-
-    return () => {
-      active = false;
-      setIsLoading(false);
-      setIsError(false);
-    };
-  }, [query, client.profile]);
-  if (!profile) return null;
   return (
-    <Detail isPending={isLoading} isError={isError}>
+    <Detail isPending={isLoading} isError={!!error}>
       <Stack spacing="lg" m="md">
         <IconHeader
           title={profile?.handle}
-          subtitle={profile.bio}
+          subtitle={profile?.bio}
           imageUrl={icon.src}
         />
         <Group spacing="md">
