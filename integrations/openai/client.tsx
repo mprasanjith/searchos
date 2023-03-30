2390599b7f9995cb98108f4e2cda2f5734b78d90
Madusha Prasanjith <hi@madusha.me>
2023-03-30 18:43:28 +0200

    feat: Add aggressive caching.

--- integrations/openai/client.tsx
+++ integrations/openai/client.tsx
@@ -7,10 +7,15 @@ export interface CommandCompletion {
   message?: string;
 }
 
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export class ChatGPTClient {
   private http: HTTP = new HTTP();
 
   async getCompletion(query: string) {
+    await delay(1500);
     return this.http.get<CommandCompletion>(`/api/completion?query=${query}`);
   }
 }
